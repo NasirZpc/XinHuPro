00248ff1767cff8cef9526e1ed3472f3fc2710bd
NasirZpc <1041870188@qq.com>
2018-07-19 17:04:58 +0800

    底部导航

--- components/Footer.vue
+++ components/Footer.vue
@@ -1,6 +1,37 @@
 <template>
     <footer class="fix">
-        <ul></ul>
+        <ul>
+            <li>
+                <nuxt-link to="/">
+                    <i class="icon i_index"></i>
+                    <p>首页</p>
+                </nuxt-link>
+            </li>
+            <li>
+                <nuxt-link to="/xinFind">
+                    <i class="icon i_find"></i>
+                    <p>信发现</p>
+                </nuxt-link>
+            </li>
+            <li>
+                <nuxt-link to="/serverMsg">
+                    <i class="icon i_serverMsg"></i>
+                    <p>客服消息</p>
+                </nuxt-link>
+            </li>
+            <li>
+                <nuxt-link to="/shoppingCar">
+                    <i class="icon i_shoppingCar"></i>
+                    <p>购物车</p>
+                </nuxt-link>
+            </li>
+            <li>
+                <nuxt-link to="/mine">
+                    <i class="icon i_mine"></i>
+                    <p>我的</p>
+                </nuxt-link>
+            </li>
+        </ul>
     </footer>
 </template>
 <script>
@@ -21,6 +52,19 @@ export default{
             li{
                 width:20%;
                 float:left;
+                a{
+                    display: block;
+                    text-align: center;
+                    height:1.306rem;
+                    color:#666;
+                    font-size:0.32rem;
+                    i{
+                        margin-top:0.1rem;
+                    }
+                    &.nuxt-link-exact-active{
+                        color:#ff5c00;
+                    }
+                }
             }
         }
     }
