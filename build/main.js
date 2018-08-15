474c39ae0c313f83e67a250894bc9ab5d81777b6
NasirZpc <1041870188@qq.com>
2018-08-15 09:49:42 +0800

    设置cookie过期时间

--- build/main.js
+++ build/main.js
@@ -340,7 +340,7 @@ var start = function () {
                                                         ctx.cookies.set('userinfo', __WEBPACK_IMPORTED_MODULE_5_qs___default.a.stringify(res.data.data), {
                                                             domain: '127.0.0.1', // 写cookie所在的域名
                                                             path: '/', // 写cookie所在的路径
-                                                            maxAge: 10 * 60 * 1000, // cookie有效时长
+                                                            maxAge: 60 * 60 * 24, // cookie有效时长
                                                             // expires: new Date('2018-07-27'),  // cookie失效时间
                                                             httpOnly: true, // 是否只用于http请求中获取
                                                             overwrite: false // 是否允许重写
--- server/index.js
+++ server/index.js
@@ -44,7 +44,7 @@ async function start () {
                         {
                         domain: '127.0.0.1',  // 写cookie所在的域名
                         path: '/',       // 写cookie所在的路径
-                        maxAge: 10 * 60 * 1000, // cookie有效时长
+                        maxAge: 60*60*24, // cookie有效时长
                         // expires: new Date('2018-07-27'),  // cookie失效时间
                         httpOnly: true,  // 是否只用于http请求中获取
                         overwrite: false  // 是否允许重写
